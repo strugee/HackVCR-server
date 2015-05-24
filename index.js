9795bace8ee3ffd01da639a983104a0bcbf5e00f
Alex Jordan <alex@strugee.net>
2015-05-24 08:35:43 -0700

    Add basic protocol parsing

--- index.js
+++ index.js
@@ -5,24 +5,48 @@
 var fs = require('fs');
 var net = require('net');
 
+var endcmds = ['VERSION', 'CAPABILITY', 'END'];
+
 var sink = process.stdout;
 var ctlpath = '/tmp/' + process.getuid() + '-hackvcr.sock';
-
 var ctl = net.createServer(function(con) {
 	console.log('Client connected.');
+	
+	var cmdbuf = '';
 	
 	con.on('end', function() {
 		console.log('Client disconnected.');
 	});
-	
-	con.on('data', function(str) {
-		sink.write(str);
-	});
-	
+		
 	con.write('HACKVCR\n');
 	con.write('VERSION 0.1\n');
 	con.write('CAPABILITY EDITOR_FILE\n');
 	con.write('END\n');
+	
+	con.on('data', function(str) {
+		str = str.toString();
+		cmdbuf = cmdbuf + str;
+		
+		cmdbuf = cmdbuf.trim() + '\n';
+
+		// TODO: this code has subtle race conditions if two commands come through at once
+
+		for (var i in endcmds) {
+			
+			var match = cmdbuf.match(new RegExp('^[^]*\n' + endcmds[i] + '.*$', 'm'));
+			if (match != null) {
+				if (match.length != 1) {
+					console.log(match);
+					throw new Error();
+				}
+				var result = {};
+				result.raw = match[0];
+				result.processed = match[0].replace(endcmds[i], '').trim();
+				result.method = endcmds[i];
+				console.log(result);
+			}
+		}
+	});
 });
 
 ctl.listen(ctlpath, function() {
